185b76c82b4e0a99ca96989f4c322db1409d0b1f
jhought2 <111276946+jhought2@users.noreply.github.com>
2022-08-18 09:25:09 +0100

    Update NotebookPostgres.py

--- NotebookPostgres.py
+++ NotebookPostgres.py
@@ -9,7 +9,7 @@ import pandas as pd
 # Uses st.experimental_singleton to only run once.
 @st.experimental_singleton
 def init_connection():
-    return psycopg2.connect(**st.secrets["postgres"])
+    return psycopg2.connect(**st.secrets["streamlit"])
 
 conn = init_connection()
 
@@ -46,7 +46,7 @@ Note=st.text_input("What's your note?", "Note")
 
 #Record data into database
 cur = conn.cursor()
-cur.execute("INSERT INTO notebook ("Date" , "NoteType", "NoteTitle", "Note") VALUES (%s, %s, %s, %s);", (Date, NoteType, NoteTitle, Note))
+cur.execute("INSERT INTO notebook (Date , NoteType, NoteTitle, Note) VALUES (%s, %s, %s, %s);", (Date, NoteType, NoteTitle, Note))
 
 
 #cur.execute('INSERT INTO Notes (Date , NoteType, NoteTitle, Note) VALUES (?, ?, ?, ?)',
